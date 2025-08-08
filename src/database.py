fb563c7ea083b8cbcc125adada835fb223d30677
Kabilesh <2022504541@student.annauniv.edu>
2025-08-08 16:03:24 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,1 +1,20 @@
+import mysql.connector
+class Database:
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host="localhost", user="root", passwd="Kabilesh1!", database="sms_database"
+        )
+        self.cursor = self.conn.cursor()
 
+    def execute(self, query, values=None):
+        self.cursor.execute(query, values or ())
+        return self.cursor
+
+    def commit(self):
+        self.conn.commit()
+
+    def fetch_all(self):
+        return self.cursor.fetchall()
+
+    def close(self):
+        self.conn.close()
